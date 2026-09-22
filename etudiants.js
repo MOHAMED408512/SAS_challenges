829553cc3494b539509e4465d7842bdd812749c3
MOHAMED408512 <sdhmdharrt@gmail.com>
2026-09-22 12:29:22 +0100

    Add files via upload

---
+++ etudiants.js
@@ -0,0 +1,7 @@
+let names = ["harry" , "rim" , "zayna" ,"ali" , "nani" ,"nasser"]
+for ( let i = 0 ; i < names.length ; i++ ){
+    if (names[i].includes("a")) {
+        console.log(names[i])
+    }
+}
+
