6b1cbad7051d8fd56602499a70aa43f389ece3dd
MOHAMED408512 <sdhmdharrt@gmail.com>
2026-09-22 17:24:30 +0100

    Add files via upload

---
+++ objects1.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+let personne = {
+    nom :"harry" ,
+    prenom :"mohamed" ,
+    Age: 18
+}
+
+console.log(`nom de personne ${personne.nom}`)
+console.log(`prenom de personne ${personne.prenom}`)
+console.log(`Age du personne ${personne.Age}`)
