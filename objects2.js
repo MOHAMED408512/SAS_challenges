58c609cc96c1593557e2fd7965ea680d17a3b303
MOHAMED408512 <sdhmdharrt@gmail.com>
2026-09-22 17:58:26 +0100

    Add files via upload

---
+++ objects2.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let etudiant = { 
+    nom:"harry",
+    prenom:"sd7mad",
+tableauDeNote:[8,18,2,20]
+}
+console.log(`nom de etudiant:${etudiant.nom}`)
+console.log(`prenom de etudiant:${etudiant.prenom}`)
+console.log(`tableauDeNote de etudiant:${etudiant.tableauDeNote}`)
