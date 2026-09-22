275d7b730445c0f995c9e6f00163782fcd1c7ac8
MOHAMED408512 <sdhmdharrt@gmail.com>
2026-09-22 12:49:54 +0100

    Add files via upload

---
+++ harry.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let nom = prompt ("ton nom ?")
+let prenom = prompt ("ton prenom ?")
+let age = prompt ("ton age ?")
+let sexe = prompt ("ton sexe ?")
+let adressEmail = prompt("ton adresse email ?")
+console.log("$(nom) $(prenom) $(age) $(sexe) $(email)")
+
+
