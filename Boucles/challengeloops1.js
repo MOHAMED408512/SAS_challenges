0124fd863067d8f01aa65aa24423409b807324c0
MOHAMED408512 <sdhmdharrt@gmail.com>
2026-09-23 14:56:11 +0200

    Delete Boucles directory

--- Boucles/challengeloops1.js
+++
@@ -1,8 +0,0 @@
-const prompt = require('prompt-sync')();
-let a = parseInt(prompt(`enter a number:`))
-console.log(` tableau de multiplicaion de ${a} :`)
-for (let i = 1; i <= 10; i++) {
-    let resultat = a * i;
-    
-console.log(`${a} * ${i} = ${resultat}`)}
-;
