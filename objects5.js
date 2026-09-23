dd0bdd825fd70c711ef05111d076866254305852
MOHAMED408512 <sdhmdharrt@gmail.com>
2026-09-23 11:44:14 +0100

    Add files via upload

---
+++ objects5.js
@@ -0,0 +1,7 @@
+let livre = {
+    titre: " jackson storm ",
+    auteur : " lightning mqueen",
+    annee : " 2006 "
+}
+let monLivre = (livre.titre + livre.auteur + livre.annee)
+console.log(monLivre)
